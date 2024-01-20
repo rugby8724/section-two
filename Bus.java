c37f8942a05101fcda8af9cf03d70734d869b846
rugby8724 <jeremiah.d.wise@gmail.com>
2024-01-19 18:27:28 -0600

    First Commit

---
+++ Bus.java
@@ -0,0 +1,9 @@
+public class Bus {
+
+  public static void main(String[] args) {
+    int passengers = 0;
+    passengers += 9;
+    System.out.println(passengers);
+  }
+  
+}
---
+++ Decimals.java
@@ -0,0 +1,6 @@
+public class Decimals {
+  public static void main(String[] args) {
+    double percentage = 73.5;
+    System.out.println(percentage);
+  }
+}
